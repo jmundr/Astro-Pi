a23c34241b763e9195604edb13108e66302b6a1f
jmundr <31898096+jmundr@users.noreply.github.com>
2022-06-28 11:49:27 +0100

    Added comments

--- calculate_beauty.py
+++ calculate_beauty.py
@@ -1,4 +1,5 @@
 def calculate_beauty(file_name):
+    # Read the file from the folder
     img = cv2.imread(f'realImages/{file_name}')
 
     # Mask a circle
@@ -6,7 +7,10 @@ def calculate_beauty(file_name):
     cv2.circle(circle_mask, (img.shape[1] // 2 + 200, img.shape[0] // 2 + 50), img.shape[0] // 2 - 150, 1, -1)
     img = cv2.bitwise_and(img, img, mask=circle_mask)
 
+    # Convert all colours to HSV
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    # Define the colour ranges in HSV
     lower_blue = np.array([90, 50, 50])
     upper_blue = np.array([130, 255, 255])
     lower_green = np.array([20, 20, 50])
@@ -16,11 +20,13 @@ def calculate_beauty(file_name):
     lower_black = np.array([0, 0, 0])
     upper_black = np.array([180, 255, 31])
 
+    # Filter out every colour but the ones in the specified rangee
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
     mask_white = cv2.inRange(hsv, lower_white, upper_white)
     mask_green = cv2.inRange(hsv, lower_green, upper_green)
     mask_black = cv2.inRange(hsv, lower_black, upper_black)
 
+    # Grayscale the masks
     blue_result = cv2.bitwise_and(img, img, mask=mask_blue)
     blue_grayscale = cv2.cvtColor(blue_result, cv2.COLOR_BGR2GRAY)
     white_result = cv2.bitwise_and(img, img, mask=mask_white)
@@ -33,6 +39,7 @@ def calculate_beauty(file_name):
     rows, columns, _ = img.shape
     total_area = int(rows) * int(columns)
 
+    # Calculate the amount of non-black pixels in the image (These pixels represent the colours specified in the ranges)
     blue_area = cv2.countNonZero(blue_grayscale)
     perc_blue = round(((blue_area / total_area) * 100), 3)
 
@@ -47,12 +54,15 @@ def calculate_beauty(file_name):
     black_area = cv2.countNonZero(black_grayscale) + zero_area
     perc_black = round(((black_area / total_area) * 100), 3)
 
+    # Calculate the saliency of the image and how far this is away from a third
     saliency_score = calc_thirds(img)
 
+    # Calculate the total beauty of the image
     def total_image_beauty_rating(perc_blue, perc_white, perc_green, perc_black, saliency_score):
         return 100 + 2 * (-abs(perc_blue - perc_green) - 2 * abs(
             saliency_score) + perc_blue + 2 * perc_green - 3 * perc_white - 2 * (perc_black - 57))
 
     total_score = total_image_beauty_rating(perc_blue, perc_white, perc_green, perc_black, saliency_score)
 
+    # Return the total score
     return total_score
